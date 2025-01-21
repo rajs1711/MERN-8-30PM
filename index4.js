980fc6e5030fe3cb8a68436855327bb8ee408720
saurav <rajsaurav1711@gmail.com>
2025-01-21 20:17:45 +0530

    new files

---
+++ index4.js
@@ -0,0 +1,19 @@
+//Example : anonymous function
+(
+    ()=>{
+        // anonymous function
+    }
+)
+
+function (){
+    // anonymous function
+}
+
+//IIFE   function ( Immediatly invoked function expression)
+Another use of anonymous function.
+(
+    function(){
+      console.log('Hello I am IIFE function ')
+    }
+)()
+
